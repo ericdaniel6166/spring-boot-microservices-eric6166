dadd9623c43c47c3bc725e49fbe88df6d5fcea52
eric6166 <ericnguyen6166@gmail.com>
2024-04-25 16:33:33 +0700

    commit

--- common-service/src/main/java/com/eric6166/common/CommonServiceApplication.java
+++ common-service/src/main/java/com/eric6166/common/CommonServiceApplication.java
@@ -2,7 +2,6 @@ package com.eric6166.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommonServiceApplication {
