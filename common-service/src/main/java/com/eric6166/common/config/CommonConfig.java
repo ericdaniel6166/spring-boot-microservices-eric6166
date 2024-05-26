f628016d7df90363b3083e6eb4e6352d162e41fc
eric6166 <ericnguyen6166@gmail.com>
2024-05-26 23:11:27 +0700

    commit

--- common-service/src/main/java/com/eric6166/common/config/CommonConfig.java
+++ common-service/src/main/java/com/eric6166/common/config/CommonConfig.java
@@ -3,10 +3,11 @@ package com.eric6166.common.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ModelMapperConfig {
+@Configuration
+public class CommonConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
