b8dbd753c15a6587f60305449b93dfc297186be1
eric6166 <ericnguyen6166@gmail.com>
2024-06-09 19:34:29 +0700

    commit

--- jpa-service/src/main/java/com/eric6166/jpa/model/BaseEntity.java
+++ jpa-service/src/main/java/com/eric6166/jpa/model/BaseEntity.java
@@ -3,9 +3,11 @@ package com.eric6166.jpa.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
@@ -23,23 +25,24 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @SuperBuilder
+@FieldDefaults(level = AccessLevel.PROTECTED)
 public abstract class BaseEntity<U> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @CreatedBy
     @Column(name = "CREATED_BY")
-    protected U createdBy;
+    U createdBy;
 
     @CreatedDate
     @Column(name = "CREATED_DATE")
-    protected LocalDateTime createdDate;
+    LocalDateTime createdDate;
 
     @LastModifiedBy
     @Column(name = "LAST_MODIFIED_BY")
-    protected U lastModifiedBy;
+    U lastModifiedBy;
 
     @LastModifiedDate
     @Column(name = "LAST_MODIFIED_DATE")
-    protected LocalDateTime lastModifiedDate;
+    LocalDateTime lastModifiedDate;
 }
