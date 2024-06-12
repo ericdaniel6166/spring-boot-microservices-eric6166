cc0db35fd4ef024dd2c537a6cc3cdeeb72a68801
eric6166 <ericnguyen6166@gmail.com>
2024-06-12 14:08:36 +0700

    commit

--- jpa-service/src/main/java/com/eric6166/jpa/utils/PageUtils.java
+++ jpa-service/src/main/java/com/eric6166/jpa/utils/PageUtils.java
@@ -7,11 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 public final class PageUtils {
