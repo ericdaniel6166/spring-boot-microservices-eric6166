2f1f9517564fb1b4430247ee91ca39a3505c94e3
eric6166 <ericnguyen6166@gmail.com>
2024-06-21 14:18:41 +0700

    commit

--- keycloak-admin-client-service/src/main/java/com/eric6166/keycloak/validation/UserValidation.java
+++ keycloak-admin-client-service/src/main/java/com/eric6166/keycloak/validation/UserValidation.java
@@ -1,20 +1,11 @@
 package com.eric6166.keycloak.validation;
 
-import com.eric6166.base.exception.AppValidationException;
-import com.eric6166.base.exception.ValidationErrorDetail;
-import com.eric6166.base.utils.BaseConst;
-import com.eric6166.base.utils.BaseMessageConst;
 import com.eric6166.keycloak.config.KeycloakAminClient;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
@@ -22,27 +13,18 @@ import java.util.Collections;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserValidation {
 
-    MessageSource messageSource;
     KeycloakAminClient keycloakAminClient;
 
-    public void validateUsernameExisted(String username) throws AppValidationException {
-        log.info("UserValidation.validateUsernameExisted"); // comment // for local testing
-        var searchByUsername = keycloakAminClient.searchUserByUsername(username);
-        if (searchByUsername.isPresent()) {
-            var res = messageSource.getMessage(BaseMessageConst.MGS_RES_USERNAME, null, LocaleContextHolder.getLocale());
-            var msg = messageSource.getMessage(BaseMessageConst.MSG_ERR_RESOURCE_EXISTED, new String[]{res}, LocaleContextHolder.getLocale());
-            throw new AppValidationException(Collections.singletonList(new ValidationErrorDetail(BaseConst.FIELD_USERNAME, StringUtils.capitalize(msg))));
-        }
+
+    public boolean isUsernameExisted(String username) {
+        log.info("UserValidation.isUsernameExisted"); // comment // for local testing
+        return keycloakAminClient.searchUserByUsername(username).isPresent();
     }
 
-    public void validateEmailExisted(String email) throws AppValidationException {
-        log.info("UserValidation.validateEmailExisted"); // comment // for local testing
-        var searchByEmail = keycloakAminClient.searchUserByEmail(email);
-        if (searchByEmail.isPresent()) {
-            var res = messageSource.getMessage(BaseMessageConst.MGS_RES_EMAIL, null, LocaleContextHolder.getLocale());
-            var msg = messageSource.getMessage(BaseMessageConst.MSG_ERR_RESOURCE_EXISTED, new String[]{res}, LocaleContextHolder.getLocale());
-            throw new AppValidationException(Collections.singletonList(new ValidationErrorDetail(BaseConst.FIELD_EMAIL, StringUtils.capitalize(msg))));
-        }
+    public boolean isEmailExisted(String email) {
+        log.info("UserValidation.isEmailExisted"); // comment // for local testing
+        return keycloakAminClient.searchUserByEmail(email).isPresent();
+
     }
 
 
