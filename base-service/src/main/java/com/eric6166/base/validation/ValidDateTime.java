9890dca628cbb7ceef931ea29bdd6aa12de2dab7
eric6166 <ericnguyen6166@gmail.com>
2024-07-12 15:44:55 +0700

    commit

--- base-service/src/main/java/com/eric6166/base/validation/ValidDateTime.java
+++ base-service/src/main/java/com/eric6166/base/validation/ValidDateTime.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(
