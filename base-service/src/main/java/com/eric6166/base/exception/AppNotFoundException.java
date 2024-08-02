29f1360832f221c5c10606697fc78885c5ad0b53
eric6166 <ericnguyen6166@gmail.com>
2024-08-02 20:24:42 +0700

    commit

--- base-service/src/main/java/com/eric6166/base/exception/AppNotFoundException.java
+++ base-service/src/main/java/com/eric6166/base/exception/AppNotFoundException.java
@@ -6,8 +6,13 @@ import org.springframework.http.HttpStatus;
 public class AppNotFoundException extends AppException {
 
     public AppNotFoundException(String resource) {
-        super(HttpStatus.NOT_FOUND, StringUtils.isNotBlank(resource) ? String.format("%s not found", resource) : HttpStatus.NOT_FOUND.getReasonPhrase());
+        super(HttpStatus.NOT_FOUND,
+                StringUtils.isNotBlank(resource) ? String.format("%s not found", resource) : HttpStatus.NOT_FOUND.getReasonPhrase());
     }
 
-
+    public AppNotFoundException(String resource, Object rootCause) {
+        super(HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.name(),
+                StringUtils.isNotBlank(resource) ? String.format("%s not found", resource) : HttpStatus.NOT_FOUND.getReasonPhrase(),
+                rootCause);
+    }
 }
--- base-service/src/main/java/com/eric6166/base/exception/AppValidationException.java
+++ base-service/src/main/java/com/eric6166/base/exception/AppValidationException.java
@@ -1,15 +1,14 @@
 package com.eric6166.base.exception;
 
-import java.util.List;
 
 public class AppValidationException extends AppException {
 
-    public AppValidationException(List<ErrorDetail> errorDetails) {
-        super(ErrorCode.VALIDATION_ERROR.getHttpStatus(), ErrorCode.VALIDATION_ERROR.name(), ErrorCode.VALIDATION_ERROR.getReasonPhrase(), errorDetails);
+    public AppValidationException(String message) {
+        super(ErrorCode.VALIDATION_ERROR.getHttpStatus(), ErrorCode.VALIDATION_ERROR.name(), message);
     }
 
-    public AppValidationException(String message) {
-        super(ErrorCode.VALIDATION_ERROR.getHttpStatus(), message);
+    public AppValidationException(String message, Object rootCause) {
+        super(ErrorCode.VALIDATION_ERROR.getHttpStatus(), ErrorCode.VALIDATION_ERROR.name(), message, rootCause);
     }
 
 }
